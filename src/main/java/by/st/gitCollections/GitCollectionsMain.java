1e5218d9c1f62a45028fc106358b95a8f831aeb5
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2024-08-28 20:34:14 +0300

    add new line

--- src/main/java/by/st/gitCollections/GitCollectionsMain.java
+++ src/main/java/by/st/gitCollections/GitCollectionsMain.java
@@ -8,6 +8,7 @@ import java.util.Stack;
 public class GitCollectionsMain {
     public static void main(String[] args) {
         System.out.println("MyArrayList");
+        System.out.println("MyLinkedList");
 
         System.out.println("MyArrayList");
         System.out.println("MyLinkedList");
